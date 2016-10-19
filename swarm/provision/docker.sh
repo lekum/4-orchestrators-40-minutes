61c05ae5344273a5738af5abda7ae6a9f354b535
Alejandro Guirao <lekumberri@gmail.com>
2016-10-19 13:49:07 +0200

    Fix dockerd restart

--- swarm/provision/docker.sh
+++ swarm/provision/docker.sh
@@ -8,5 +8,5 @@ apt-get update
 apt-get install -y linux-image-extra-$(uname -r) linux-image-extra-virtual docker-engine
 sed -i 's#ExecStart=.*#ExecStart=/usr/bin/dockerd -H tcp://0.0.0.0:2375#' /lib/systemd/system/docker.service
 systemctl daemon-reload
-service docker start
+service docker restart
 adduser ubuntu docker
